3b8fd38fc9615d0df9b6a3f91ea0c7a2f93529dd
aledamen <alejandrodamen3@gmail.com>
2022-10-25 20:40:01 -0300

    fix bugs to deploy

--- client/product/api.ts
+++ client/product/api.ts
@@ -5,7 +5,7 @@ import Papa from 'papaparse'
 
 export default{
     list: async ():Promise<Product[]> => {
-        return await axios.get(process.env.PATH_CSV as string, { responseType: 'blob' }).then((res) => {
+        return await axios.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vRjtu6BTsnobH-sjz78yHNNhsSaw9S4DiNgWryLAhy5Kk-rxWcYsCOsAmx4n7BOLyCOZUAzhpIartAD/pub?gid=0&single=true&output=csv' as string, { responseType: 'blob' }).then((res) => {
             return new Promise<Product[]>((resolve, reject) => {
                 Papa.parse(res.data, {
                     header: true,
